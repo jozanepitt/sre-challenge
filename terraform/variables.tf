e012108e889a72c2dff4f746e619167d7eb39621
jozanepitt <69273769+jozanepitt@users.noreply.github.com>
2024-10-18 17:58:46 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,19 @@
 variable "aws_region" {
-  description = "The AWS region to deploy in"
+  description = "AWS region to deploy the EKS cluster"
   default     = "us-east-1"
 }
 
+variable "desired_node_size" {
+  description = "Desired number of worker nodes"
+  default     = 2
+}
+
+variable "max_node_size" {
+  description = "Maximum number of worker nodes"
+  default     = 3
+}
+
+variable "min_node_size" {
+  description = "Minimum number of worker nodes"
+  default     = 1
+}
