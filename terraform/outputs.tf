4734722c16c9564d0b5745ddb1370464f518c743
jozanepitt <69273769+jozanepitt@users.noreply.github.com>
2024-10-18 17:59:10 +0200

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,5 +1,17 @@
+# EKS Cluster Endpoint
 output "eks_cluster_endpoint" {
   description = "EKS cluster endpoint"
-  value       = aws_eks_cluster.eks.endpoint
+  value       = aws_eks_cluster.sre_challenge_eks_cluster.endpoint
 }
 
+# EKS Cluster ARN
+output "eks_cluster_arn" {
+  description = "EKS cluster ARN"
+  value       = aws_eks_cluster.sre_challenge_eks_cluster.arn
+}
+
+# EKS Node Group Name
+output "eks_node_group_name" {
+  description = "EKS Node Group Name"
+  value       = aws_eks_node_group.sre_challenge_node_group.node_group_name
+}
