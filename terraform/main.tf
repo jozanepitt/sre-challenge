3cac9d3854fbb1f0441ac9c86859b5ca83239d2f
jozanepitt <69273769+jozanepitt@users.noreply.github.com>
2024-10-18 17:54:32 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,27 +2,46 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_vpc" "main" {
+# VPC
+resource "aws_vpc" "sre_challenge_vpc" {
   cidr_block = "10.0.0.0/16"
-}
-
-resource "aws_subnet" "public_subnet" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
-}
-
-resource "aws_eks_cluster" "eks" {
-  name     = "sre-challenge-cluster"
-  role_arn = aws_iam_role.eks_role.arn
-
-  vpc_config {
-    subnet_ids = [aws_subnet.public_subnet.id]
+  tags = {
+    Name = "sre-challenge-vpc"
+    Environment = "dev"
   }
 }
 
-resource "aws_iam_role" "eks_role" {
-  name = "eks-role"
+# Public Subnet
+resource "aws_subnet" "sre_challenge_subnet" {
+  vpc_id            = aws_vpc.sre_challenge_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "sre-challenge-subnet"
+    Environment = "dev"
+  }
+}
+
+# EKS Cluster
+resource "aws_eks_cluster" "sre_challenge_eks_cluster" {
+  name     = "sre-challenge-eks-cluster"
+  role_arn = aws_iam_role.sre_challenge_eks_role.arn
+
+  vpc_config {
+    subnet_ids = [aws_subnet.sre_challenge_subnet.id]
+  }
+
+  tags = {
+    Name = "sre-challenge-eks-cluster"
+    Environment = "dev"
+  }
+}
+
+# IAM Role for EKS
+resource "aws_iam_role" "sre_challenge_eks_role" {
+  name = "sre-challenge-eks-role"
 
   assume_role_policy = jsonencode({
     "Version": "2012-10-17",
@@ -35,5 +54,9 @@ resource "aws_iam_role" "eks_role" {
       "Sid": ""
     }]
   })
-}
 
+  tags = {
+    Name = "sre-challenge-eks-role"
+    Environment = "dev"
+  }
+}
